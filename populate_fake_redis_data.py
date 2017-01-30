f7ef68c0161df9c9b5d6bd18e7d89ef19fd1f323
Anne LoVerso <aelv13@gmail.com>
2017-01-30 11:47:55 -0500

    fix double quotes in fake redis maker

--- populate_fake_redis_data.py
+++ populate_fake_redis_data.py
@@ -3,5 +3,5 @@ import redis
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 for i in range(20):
-	m = json.dumps({'subject':'subject'+str(i), 'size':0})
+	m = json.dumps({"subject":'subject'+str(i), "size":0})
 	r.rpush('messages', m)
