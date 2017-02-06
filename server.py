d593e026656e7f1f18ab976c858773cb7178c131
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2017-02-05 19:48:25 -0500

    add sentiment analysis to collector

--- server.py
+++ server.py
@@ -1,31 +1,32 @@
-from flask import Flask, render_template
-from flask import jsonify
 import json
 import os
 import redis
+
+from flask import Flask, render_template
+from flask import jsonify
 
 app = Flask(__name__)
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/nextmessage')
 def get_next_message():
-	# assuming we are storing messages in a redis list called messages
-	# rpop removes right-end of list (least-recently added, queue)
-	new_message = r.rpop('messages')
-	resend_old_message = False
+    # assuming we are storing messages in a redis list called messages
+    # rpop removes right-end of list (least-recently added, queue)
+    new_message = r.rpop('messages')
+    resend_old_message = False
 
-	if new_message==None:
-		new_message = r.get('lastmessage')
-		resend_old_message = True
+    if new_message==None:
+        new_message = r.get('lastmessage')
+        resend_old_message = True
 
-	r.set('lastmessage', new_message)
-	new_message_json = json.loads(new_message.decode(encoding='UTF-8'))
-	new_message_json['resend_old_message'] = resend_old_message
-	return jsonify(new_message_json)
+    r.set('lastmessage', new_message)
+    new_message_json = json.loads(new_message.decode(encoding='UTF-8'))
+    new_message_json['resend_old_message'] = resend_old_message
+    return jsonify(new_message_json)
 
 if __name__ == '__main__':
     app.run(debug=True)
