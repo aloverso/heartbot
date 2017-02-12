19df89a8940ecc1748a9bedc32f0989441e67549
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2017-02-12 16:26:27 -0500

    tune sound slightly - still not great

--- populate_fake_redis_data.py
+++ populate_fake_redis_data.py
@@ -1,7 +1,8 @@
 import json
 import redis
+import random
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 for i in range(20):
-	m = json.dumps({"subject":'subject'+str(i), "size":0})
-	r.rpush('messages', m)
+    m = json.dumps({"subject":'subject'+str(i), "size":random.uniform(1000, 1000000), 'sentiment': random.uniform(-1, 1)})
+    r.rpush('messages', m)
